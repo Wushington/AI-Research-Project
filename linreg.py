7129533753867cc829bee4858d086cacb6718878
Jonathan Wu <Jtzw21@gmail.com>
2024-03-11 12:06:47 -0400

    update csv and py file

--- linreg.py
+++ linreg.py
@@ -4,5 +4,4 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 
-df = pd.read_xlsx('Combined Data.xlsx')
-print(df)
+df = pd.read_csv('data\Combined Data.csv')
