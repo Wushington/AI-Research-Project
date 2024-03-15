5f8ecf6a875a3c3200d6ffa1b6ae14e5ad5bc3de
Jonathan Wu <Jtzw21@gmail.com>
2024-03-14 22:00:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -78,25 +78,4 @@ total = (hw * (hw_m)) + (proj * (proj_m)) + (quiz * (quiz_m)) + (exam * (exam_m)
 final = m * total + b
 letter = 'A' if final >= 90 else 'B' if final >= 80 else 'C' if final >= 70 else 'D' if final >= 60 else 'F'
 
-print(f'\nPredicted final grade: {final:.2f} ({letter})')
-# Load the data
-data = pd.read_csv('data//FinalData.csv')
-
-data_features = data[['Project', 'Quiz', 'Exam']]
-X = data[data_features]
-y = data.Homework
-
-plt.subplot(2, 1, 1)
-plt.scatter(X['Project'], y)
-plt.subplot(2, 1, 2)
-plt.scatter(X['Quiz'], y)
-plt.subplot(2, 1, 3)
-plt.scatter(X['Exam'], y)
-
-# def loss_function(m, b, points):
-#     totalError = 0
-#     for i in range(0, len(points)):
-#         x = points[i, 0]
-#         y = points[i, 1]
-#         totalError += (y - (m * x + b)) ** 2
-#     return totalError / float(len(points))
+print(f'Predicted final grade: {final:.2f} ({letter})')
