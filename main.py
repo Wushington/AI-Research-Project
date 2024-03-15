5d40055585ad8467e24357d5a0ac52d166ce28c9
Jonathan Wu <Jtzw21@gmail.com>
2024-03-15 11:49:04 -0400

    update dataset and use total for linear regression

--- main.py
+++ main.py
@@ -23,36 +23,44 @@ def gradient_descent(m, b, points, learning_rate, column):
 
 # Initialize variables
 L = .0001
-epochs = 500
+epochs = 1000
+
+m = 0
+b = 0
+for i in range(epochs):
+    if i % 100 == 0:
+        print(f'Epoch {i}')
+    m, b = gradient_descent(m, b, data, L, 'Total')
+print(f'Total Weights: (m={m}, b={b})')
 
 # Perform gradient descent
     # Homework
-hw_m = 0
-hw_b = 0
-for i in range(epochs):
-    hw_m, hw_b = gradient_descent(hw_m, hw_b, data, L, 'Homework')
-print(f'Homework Weights: (m={hw_m}, b={hw_b})')
+# hw_m = 0
+# hw_b = 0
+# for i in range(epochs):
+#     hw_m, hw_b = gradient_descent(hw_m, hw_b, data, L, 'Homework')
+# print(f'Homework Weights: (m={hw_m}, b={hw_b})')
 
-    # Project
-proj_m = 0
-proj_b = 0
-for i in range(epochs):
-    proj_m, proj_b = gradient_descent(proj_m, proj_b, data, L, 'Project')
-print(f'Project Weights: (m={proj_m}, b={proj_b})')
+#     # Project
+# proj_m = 0
+# proj_b = 0
+# for i in range(epochs):
+#     proj_m, proj_b = gradient_descent(proj_m, proj_b, data, L, 'Project')
+# print(f'Project Weights: (m={proj_m}, b={proj_b})')
 
-    # Quiz
-quiz_m = 0
-quiz_b = 0
-for i in range(epochs):
-    quiz_m, quiz_b = gradient_descent(quiz_m, quiz_b, data, L, 'Quiz')
-print(f'Quiz Weights: (m={quiz_m}, b={quiz_b})')
+#     # Quiz
+# quiz_m = 0
+# quiz_b = 0
+# for i in range(epochs):
+#     quiz_m, quiz_b = gradient_descent(quiz_m, quiz_b, data, L, 'Quiz')
+# print(f'Quiz Weights: (m={quiz_m}, b={quiz_b})')
 
-    # Exam
-exam_m = 0
-exam_b = 0
-for i in range(epochs):
-    exam_m, exam_b = gradient_descent(exam_m, exam_b, data, L, 'Exam')
-print(f'Exam Weights: (m={exam_m}, b={exam_b})')
+#     # Exam
+# exam_m = 0
+# exam_b = 0
+# for i in range(epochs):
+#     exam_m, exam_b = gradient_descent(exam_m, exam_b, data, L, 'Exam')
+# print(f'Exam Weights: (m={exam_m}, b={exam_b})')
 
 # Plot the data
 # plt.scatter(data.Total, data.Final)
@@ -64,16 +72,17 @@ print(f'Exam Weights: (m={exam_m}, b={exam_b})')
 # # proj = input('Enter the project average: ').replace(',', '.')
 # # quiz = input('Enter the quiz average: ').replace(',', '.')
 # # exam = input('Enter the exam grade: ').replace(',', '.')
-hw = 90.0
+hw = 100.0
 proj = 100.0
-quiz = 80.0
-exam = 85.0
+quiz = 100.0
+exam = 100.0
 
-total_m = hw_m + proj_m + quiz_m + exam_m
-m = total_m / 4
-b = (hw_b + proj_b + quiz_b + exam_b) / 4
+# total_m = hw_m + proj_m + quiz_m + exam_m
+# m = total_m / 4
+# b = (hw_b + proj_b + quiz_b + exam_b) / 4
+# total = (hw * (hw_m / total_m)) + (proj * (proj_m / total_m)) + (quiz * (quiz_m / total_m)) + (exam * (exam_m / total_m))
 
-total = (hw * (hw_m / total_m)) + (proj * (proj_m / total_m)) + (quiz * (quiz_m / total_m)) + (exam * (exam_m / total_m))
+total = (hw * .15) + (proj * .15) + (quiz * .2) + (exam * .5)
 final = m * total + b
 letter = 'A' if final >= 90 else 'B' if final >= 80 else 'C' if final >= 70 else 'D' if final >= 60 else 'F'
 
