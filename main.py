3fd2ede307c8803947c1b025010ad3836c970810
Jonathan Wu <Jtzw21@gmail.com>
2024-03-15 14:53:27 -0400

    Trying to make final value more accurate

--- main.py
+++ main.py
@@ -23,44 +23,45 @@ def gradient_descent(m, b, points, learning_rate, column):
 
 # Initialize variables
 L = .0001
-epochs = 1000
-
-m = 0
-b = 0
-for i in range(epochs):
-    if i % 100 == 0:
-        print(f'Epoch {i}')
-    m, b = gradient_descent(m, b, data, L, 'Total')
-print(f'Total Weights: (m={m}, b={b})')
+epochs = 300
 
 # Perform gradient descent
+    # Total
+# m = 0
+# b = 0
+# for i in range(epochs):
+#     if i % 100 == 0:
+#         print(f'Epoch {i}')
+#     m, b = gradient_descent(m, b, data, L, 'Total')
+# print(f'Total Weights: m={m}, b={b}')
+
     # Homework
-# hw_m = 0
-# hw_b = 0
-# for i in range(epochs):
-#     hw_m, hw_b = gradient_descent(hw_m, hw_b, data, L, 'Homework')
-# print(f'Homework Weights: (m={hw_m}, b={hw_b})')
+hw_m = 0
+hw_b = 0
+for i in range(epochs):
+    hw_m, hw_b = gradient_descent(hw_m, hw_b, data, L, 'Homework')
+print(f'Homework Weights: m={hw_m}, b={hw_b}')
 
-#     # Project
-# proj_m = 0
-# proj_b = 0
-# for i in range(epochs):
-#     proj_m, proj_b = gradient_descent(proj_m, proj_b, data, L, 'Project')
-# print(f'Project Weights: (m={proj_m}, b={proj_b})')
+    # Project
+proj_m = 0
+proj_b = 0
+for i in range(epochs):
+    proj_m, proj_b = gradient_descent(proj_m, proj_b, data, L, 'Project')
+print(f'Project Weights: m={proj_m}, b={proj_b}')
 
-#     # Quiz
-# quiz_m = 0
-# quiz_b = 0
-# for i in range(epochs):
-#     quiz_m, quiz_b = gradient_descent(quiz_m, quiz_b, data, L, 'Quiz')
-# print(f'Quiz Weights: (m={quiz_m}, b={quiz_b})')
+    # Quiz
+quiz_m = 0
+quiz_b = 0
+for i in range(epochs):
+    quiz_m, quiz_b = gradient_descent(quiz_m, quiz_b, data, L, 'Quiz')
+print(f'Quiz Weights: m={quiz_m}, b={quiz_b}')
 
-#     # Exam
-# exam_m = 0
-# exam_b = 0
-# for i in range(epochs):
-#     exam_m, exam_b = gradient_descent(exam_m, exam_b, data, L, 'Exam')
-# print(f'Exam Weights: (m={exam_m}, b={exam_b})')
+    # Exam
+exam_m = 0
+exam_b = 0
+for i in range(epochs):
+    exam_m, exam_b = gradient_descent(exam_m, exam_b, data, L, 'Exam')
+print(f'Exam Weights: m={exam_m}, b={exam_b}')
 
 # Predict the final grade with averages
 # # hw = input('Enter the homework average: ').replace(',', '.')
@@ -72,18 +73,20 @@ proj = 100.0
 quiz = 100.0
 exam = 100.0
 
-# total_m = hw_m + proj_m + quiz_m + exam_m
-# m = total_m / 4
-# b = (hw_b + proj_b + quiz_b + exam_b) / 4
-# total = (hw * (hw_m / total_m)) + (proj * (proj_m / total_m)) + (quiz * (quiz_m / total_m)) + (exam * (exam_m / total_m))
-
+# Predict Using Averages
+m = (hw_m * .15) + (proj_m * .15) + (quiz_m * .2) + (exam_m * .5)
+b = (hw_b * .15) + (proj_b * .15) + (quiz_b * .2) + (exam_b * .5)
 total = (hw * .15) + (proj * .15) + (quiz * .2) + (exam * .5)
 final = m * total + b
-letter = 'A' if final >= 90 else 'B' if final >= 80 else 'C' if final >= 70 else 'D' if final >= 60 else 'F'
 
+# Predict Using Total
+# total = (hw * .15) + (proj * .15) + (quiz * .2) + (exam * .5)
+# final = m * total + b
+
+letter = 'A' if final >= 90 else 'B' if final >= 80 else 'C' if final >= 70 else 'D' if final >= 60 else 'F'
 print(f'\nPredicted final grade: {final:.2f} ({letter})')
 
 # Plot the data
-plt.scatter(data.Total, data.Final)
-plt.plot(data.Total, m * data.Total + b, color='red')
-plt.show()
+# plt.scatter(data.Total, data.Final)
+# plt.plot(data.Total, m * data.Total + b, color='red')
+# plt.show()
